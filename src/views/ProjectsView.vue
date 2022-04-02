9f2fcb78dbdf17fcbbf77a1238b3028ab382d4fe
Victguo <victorguo_123@outlook.com>
2022-04-02 01:47:44 -0400

    WIP projects

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="projects">
-    <v-layout>
+    <v-layout class="main-layout">
       <v-container>
-        <v-row>
+        <v-row class="proj-row">
             <template v-for="project in projects">
                 <v-col class="proj-col" cols="6" v-bind:key="project.id">
                     <h2>{{project['proj-title']}}</h2>
                     <img class="proj-img" alt="timetable-app" :src="image(project.img)">
                     <span>{{project.description}}</span>
                     <span>Technologies Used:</span>
-                    
                     <v-layout class="tech-layout">
                         <template v-for="(technology, i) in project.technologies">
-                            <v-flex xs3 v-bind:key="i">
+                            <v-flex class="tech-list" xs3 v-bind:key="i">
                                 <div class="text-center">
                                     <v-btn icon>
                                         <v-icon>{{technology.icon}}</v-icon>
@@ -22,7 +21,13 @@
                             </v-flex>
                         </template>
                     </v-layout>
-                    
+                    <template v-if="project.github.visible">
+                        <span>View on GitHub:</span>
+                        <v-btn icon>
+                            <v-icon>mdi-github</v-icon>
+                        </v-btn>
+                    </template>
+                    <span v-else>GitHub repository is private, contact to view source code.</span>
                 </v-col>
             </template>
         </v-row>
@@ -51,19 +56,27 @@ export default {
 </script>
 
 <style scoped>
-.layout {
-  height: 100%;
-  margin: 0px;
-  justify-items: center;
-  align-items: center;
+.main-layout {
+    height: 100%;
+    padding-top: 120px;
+    justify-items: center;
+    /* align-items: center; */
 }
 .proj-col {
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
+    max-height: 664px;
 }
 .proj-img {
-    object-fit: cover;
-    width: 100%;
+    float: left;
+    object-fit: contain;
+    max-height: 424px;
+}
+.proj-row {
+    max-height: 664px;
+}
+.tech-list {
+    max-width: 36px;
 }
 .blue-text {
   color: #91C4FF;
@@ -77,7 +90,7 @@ export default {
   word-wrap: normal;
 }
 .headshot-image {
-  object-fit: cover;
+  object-fit: contain;
 }
 .home-icon {
   height: 200px;
